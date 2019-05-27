70bb5b1363b8ec5025b9b07e976c62ff56833253
yx_lm <liumeng6@asiainfo.com>
2019-05-27 10:12:41 +0800

    增加文彬环境

--- apps/common/config.js
+++ apps/common/config.js
@@ -2,6 +2,7 @@ let CONFIG = {
     // HOST: "http://152.136.133.173/etc/"
     // HOST: "http://172.18.63.173:8080/"
     // HOST: "http://172.18.63.186:8080/"
+    // HOST: "http://wenbinh2003.xicp.net/"
 
     // 测试环境
     // HOST: "http://152.136.133.192:18080/etc/"
